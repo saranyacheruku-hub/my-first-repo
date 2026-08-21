8e52f2b1f2410a56eb133e24614849def04e4d42
saranya <saranyacheruku@gmail.com>
2026-08-21 11:53:29 +0530

    21082026

--- sara.py
+++ sara.py
@@ -1,1 +1,21 @@
-print("Hello word")
+a=""
+a=bool(a)
+print(type(a))
+print(a*10+2)
+s1="hello"
+s2="hi"
+s3="""this
+is
+a 
+string"""
+s4="""this 
+is 
+also
+a string"""
+print(s1,s2)
+print(s3)
+print(s4)
+a=range(5)
+print(a)
+for x in a:
+    print(x)
